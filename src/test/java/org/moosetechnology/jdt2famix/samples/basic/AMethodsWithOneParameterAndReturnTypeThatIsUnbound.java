b35eb8c8d7c65d4f216ed2225ef15681826078fe
Tudor Girba <tudor.girba@gmail.com>
2015-08-06 12:54:08 +0200

    small renaming of a sample with unbounded types

--- src/test/java/org/moosetechnology/jdt2famix/samples/basic/AMethodsWithOneParameterAndReturnTypeThatIsUnbound.java
+++ src/test/java/org/moosetechnology/jdt2famix/samples/basic/AMethodsWithOneParameterAndReturnTypeThatIsUnbound.java
@@ -1,7 +1,7 @@
 package org.moosetechnology.jdt2famix.samples.basic;
 
 public class AMethodsWithOneParameterAndReturnTypeThatIsUnbound {
-	VariousConstructors privateStaticVoidWithVariousAttributesParameter(VariousAttributes a) {
+	VariousConstructors withVariousAttributesParameterAndVariousConstructorsReturn(VariousAttributes a) {
 		return null;
 	}
 }
