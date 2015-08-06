f62a9ffc34fbf501e8cca37fb2fee2196b47f735
Tudor Girba <tudor.girba@gmail.com>
2015-08-06 10:39:15 +0200

    extracted method with unbound types in a separate sample

---
+++ src/test/java/org/moosetechnology/jdt2famix/samples/basic/AMethodsWithOneParameterAndReturnTypeThatIsUnbound.java
@@ -0,0 +1,7 @@
+package org.moosetechnology.jdt2famix.samples.basic;
+
+public class AMethodsWithOneParameterAndReturnTypeThatIsUnbound {
+	VariousConstructors privateStaticVoidWithVariousAttributesParameter(VariousAttributes a) {
+		return null;
+	}
+}
--- src/test/java/org/moosetechnology/jdt2famix/samples/basic/VoidMethodsWithOneParameter.java
+++ src/test/java/org/moosetechnology/jdt2famix/samples/basic/VoidMethodsWithOneParameter.java
@@ -3,7 +3,6 @@ package org.moosetechnology.jdt2famix.samples.basic;
 public class VoidMethodsWithOneParameter {
 	public static void publicStaticVoidWithStringParameter(String s) {}
 	protected static void protectedStaticVoidWithIntParameter(int i) {}
-	private static void privateStaticVoidWithVariousAttributesParameter(VariousAttributes a) {}
 	static void packageStaticVoidWithStringArrayParameter(String[] s) {}
 
 	public void publicVoidWithIntArrayParameter(int[] s) {}
