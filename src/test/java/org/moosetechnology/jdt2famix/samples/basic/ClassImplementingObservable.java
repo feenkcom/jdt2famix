2d62fada5611685d5300121b327af7efa9faeaa8
Tudor Girba <tudor.girba@gmail.com>
2015-08-10 00:25:29 +0200

    better class implementing interface sample

---
+++ src/test/java/org/moosetechnology/jdt2famix/samples/basic/ClassImplementingObservable.java
@@ -0,0 +1,9 @@
+package org.moosetechnology.jdt2famix.samples.basic;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ClassImplementingObservable implements Observer {
+	public void update(Observable arg0, Object arg1) {
+	}
+}
