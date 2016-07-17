64bdcfa3522a63dea39f2d3345e35a7a6ec542c4
Tudor Girba <tudor.girba@gmail.com>
2016-07-17 14:11:56 +0200

    added rudimentary support for command line

--- src/main/java/org/moosetechnology/jdt2famix/injava/Main.java
+++ src/main/java/org/moosetechnology/jdt2famix/injava/Main.java
@@ -1,12 +1,30 @@
 package org.moosetechnology.jdt2famix.injava;
 
+import java.io.File;
+
+import org.moosetechnology.jdt2famix.Classpath;
+import org.moosetechnology.jdt2famix.JavaFiles;
+
 public class Main {
 
 	public static void main(String[] args) {
 		InJavaImporter importer = new InJavaImporter();
-		//TODO read args and trigger the importer
-		importer.runOne("src/test/java/org/moosetechnology/jdt2famix/samples/basic/SimpleInterface.java");
-		importer.exportMSE("model.mse");
+		String javaFilesRoot = new File (".").getAbsolutePath();
+		String classpathRoot = new File (".").getAbsolutePath();
+		String mseFileName = (new File (".")).getName() + ".mse";
+		if (args.length > 0) {
+			javaFilesRoot = args[0];
+			classpathRoot = args[0];
+		}
+		JavaFiles javaFiles = new JavaFiles();
+		javaFiles.deepJavaFiles(javaFilesRoot);
+		Classpath classpath = new Classpath();
+		classpath.deepJarFiles(classpathRoot);
+		System.out.println("jdt2famix - parsing started in " + javaFilesRoot);
+		importer.run(javaFiles, classpath);
+		System.out.println("jdt2famix - parsing ended");
+		importer.exportMSE(mseFileName);
+		System.out.println("jdt2famix - model exported");
 	}
 
 }
